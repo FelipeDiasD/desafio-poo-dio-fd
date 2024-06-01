690844122fc1fd6ffea63dd3236ab70950c0b456
Felipe <prado.dias@hotmail.com>
2024-06-01 14:26:24 -0300

    feat: criado classes mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import br.com.dio.felipe.desafio.dominio.Curso;
+import br.com.dio.felipe.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
     }
 }
